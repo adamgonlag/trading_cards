7c84da3f8198d5b88862812e3a7c1830a0f9758f
adamgonlag <adamgonlag@gmail.com>
2021-06-10 17:10:58 +1000

    added required gems

--- db/db.rb
+++ db/db.rb
@@ -1,4 +1,4 @@
-def run_sql(sql_query, params = []) #Take a params 2nd argument
+def run_sql(sql_query, params = [])
     connection = PG.connect(ENV['DATABASE_URL'] || {dbname: 'trading_cards'})
     connection.prepare("query", sql_query)
     results = connection.exec_prepared('query', params)
