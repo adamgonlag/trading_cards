970afc698a0ac5e0cac442bbc4393c1e40f3262b
adamgonlag <adamgonlag@gmail.com>
2021-06-12 18:26:29 +1000

    Added digest gem to allow access to Marvel API

--- main.rb
+++ main.rb
@@ -2,12 +2,38 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry' if development?
 require 'pg'
+require 'bcrypt'
+require 'httparty'
+require 'digest'
 
 enable :sessions
+
+# Databse
+require_relative './db/db'
+
+# Models
+require_relative './models/card'
+require_relative './models/character'
+require_relative './models/user'
+
+# Helper functions
+require_relative './helpers/cards_helper'
+require_relative './helpers/sessions_helper'
+
+# Controllers
+require_relative './controllers/cards_controller'
+require_relative './controllers/characters_controller'
+require_relative './controllers/sessions_controller'
+require_relative './controllers/users_controller'
 
 get '/' do
   erb :index
 end
+
+
+
+
+
 
 
 
